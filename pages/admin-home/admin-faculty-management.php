e3bc96913149f9f91041dfe9a2f6b2b3342b466d
AugustUno <johngemare@gmail.com>
2026-06-14 10:38:41 +0800

    partial edits

--- pages/admin-home/admin-faculty-management.php
+++ pages/admin-home/admin-faculty-management.php
@@ -420,37 +420,37 @@ $conn->close();
 
                         <!-- Faculty Members Section -->
                         <div class="mb-3">
-                            <label class="form-label">Faculty Members</label>
+                                                        <label class="form-label">Faculty Members</label>
                             <!-- Search Bar -->
                             <input type="text" class="form-control mb-2" placeholder="Search faculty members...">
-                            <!-- Radio Buttons for Faculty Members -->
+                            <!-- Checkboxes for Faculty Members -->
                             <div class="border rounded p-2" style="max-height: 150px; overflow-y: auto;">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="facultyMember" id="faculty1" value="1">
+                                    <input class="form-check-input" type="checkbox" name="facultyMembers[]" id="faculty1" value="1">
                                     <label class="form-check-label" for="faculty1">
                                         Minerva McGonagall
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="facultyMember" id="faculty2" value="2">
+                                    <input class="form-check-input" type="checkbox" name="facultyMembers[]" id="faculty2" value="2">
                                     <label class="form-check-label" for="faculty2">
                                         Severus Snape
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="facultyMember" id="faculty3" value="3">
+                                    <input class="form-check-input" type="checkbox" name="facultyMembers[]" id="faculty3" value="3">
                                     <label class="form-check-label" for="faculty3">
                                         Albus Dumbledore
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="facultyMember" id="faculty4" value="4">
+                                    <input class="form-check-input" type="checkbox" name="facultyMembers[]" id="faculty4" value="4">
                                     <label class="form-check-label" for="faculty4">
                                         Filius Flitwick
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="facultyMember" id="faculty5" value="5">
+                                    <input class="form-check-input" type="checkbox" name="facultyMembers[]" id="faculty5" value="5">
                                     <label class="form-check-label" for="faculty5">
                                         Pomona Sprout
                                     </label>
